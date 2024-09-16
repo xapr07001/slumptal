556a4b372980e23c51f355055af8a7c29e6bfd5c
xapr07001 <xapr07001@utb.vaxjo.se>
2024-09-16 09:39:29 +0200

    slumptal

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Random rng = new Random();
+int number = rng.Next(1,101);
+int guess = 0;
+
+for(int tries = 1;guess != number;tries++){
+    guess = int.Parse(Console.ReadLine());
+    
+    if(guess < number){
+        Console.WriteLine("Ditt tal är för litet.");
+    }
+    else if(guess > number){
+        Console.WriteLine("Ditt tal är för stort.");
+    }
+    else{
+        Console.WriteLine("Du gissade rätt och det tog " + tries+ " försök.");
+    }
+}
